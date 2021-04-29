f990cbcef3c19e3fb10d7ebb3b8506111daad977
michaelzheng1 <mzheng1@terpmail.umd.edu>
2021-04-28 23:35:20 -0400

    new edits for 2021

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 
  var typed = new Typed('#typehere', {
-    strings: ['<h1>Hello, my name is Michael </h1> a software developer <h3><div id="details"><a href="mailto:mzheng1@terpmail.umd.edu"><i class="far fa-envelope what-second-div"></i></a><a href="https://github.com/michaelzheng1" target="_blank"><i class="fab fa-github what-second-div"></i></a><a href="https://www.linkedin.com/in/michael-zheng-77937013b/" target="_blank"><i class="fab fa-linkedin what-second-div"></i></a></div></h3>'],
+    strings: ['<h2 class="title">Hello, my name is Michael </h2> <h4 class="subtitle">a software developer</h4> <h3><div id="details"><a href="mailto:mzheng1@terpmail.umd.edu"><i class="far fa-envelope what-second-div"></i></a><a href="https://github.com/michaelzheng1" target="_blank"><i class="fab fa-github what-second-div"></i></a><a href="https://www.linkedin.com/in/michael-zheng-77937013b/" target="_blank"><i class="fab fa-linkedin what-second-div"></i></a></div></h3>'],
     typeSpeed: 35,
     smartBackspace: true,
     backSpeed: 28,
